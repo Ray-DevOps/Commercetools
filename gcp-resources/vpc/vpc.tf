c487bfa2b69a038c40150f39e4c2ba6c0dad965e
RaymondAnya <94065430+Ray-DevOps@users.noreply.github.com>
2023-09-25 18:51:00 -0400

    Update vpc.tf

--- gcp-resources/vpc/vpc.tf
+++ gcp-resources/vpc/vpc.tf
@@ -3,15 +3,16 @@ name = "commercetools-vpc"
 }
 
 resource "google_compute_subnetwork" "public-subnetwork" {
-name = "public-subnetwork"
+name          = "public-subnetwork"
 ip_cidr_range = "10.2.0.0/16"
-region = "us-central1"
-network = google_compute_network.public_network.name
+region        = "us-central1"
+network       = google_compute_network.public_network.name
 }
 
 resource "google_compute_subnetwork" "private-subnetwork" {
-name = "public-subnetwork"
-ip_cidr_range = "10.1.0.0/16"
-region = "us-central1"
-network = google_compute_network.private_network.name
+name                     = "public-subnetwork"
+ip_cidr_range            = "10.1.0.0/16"
+region                   = "us-central1"
+network                  = google_compute_network.private_network.name
+private_ip_google_access = true
 }
