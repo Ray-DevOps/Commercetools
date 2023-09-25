e54f1e2bea8c77c23bc556016b0b0c551c9dcdbd
RaymondAnya <94065430+Ray-DevOps@users.noreply.github.com>
2023-09-25 18:46:12 -0400

    Create vpc.tf
    vpc for gcp cloud

---
+++ gcp-resources/vpc/vpc.tf
@@ -0,0 +1,17 @@
+resource "google_compute_network" "vpc_network" {
+name = "commercetools-vpc"
+}
+
+resource "google_compute_subnetwork" "public-subnetwork" {
+name = "public-subnetwork"
+ip_cidr_range = "10.2.0.0/16"
+region = "us-central1"
+network = google_compute_network.public_network.name
+}
+
+resource "google_compute_subnetwork" "private-subnetwork" {
+name = "public-subnetwork"
+ip_cidr_range = "10.1.0.0/16"
+region = "us-central1"
+network = google_compute_network.private_network.name
+}
