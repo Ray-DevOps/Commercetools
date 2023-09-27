f4141d473a98a961a7e3905700e5db3d68cacd5d
RaymondAnya <94065430+Ray-DevOps@users.noreply.github.com>
2023-09-26 20:09:39 -0400

    Update dns.tf

--- aws-resources/vpc/dns.tf
+++ aws-resources/vpc/dns.tf
@@ -10,3 +10,19 @@ resource "aws_route53_record" "aws" {
   ttl     = 300
   records = [aws_eip.lb.public_ip]     # this is the elastic IP address that will be assigned to the ALB infront of the EKS cluster
 }
+
+resource "aws_route53_record" "gcp" {
+  allow_overwrite = true
+  name            = "gcp.commercetools.com"
+  ttl             = 86400
+  type            = "NS"
+  zone_id         = aws_route53_zone.primary.zone_id
+
+  records = [
+    gcp.commercetools.com_servers[1],
+    gcp.commercetools.com_servers[2],
+    gcp.commercetools.com_servers[3],
+    gcp.commercetools.com_servers[4],
+    gcp.commercetools.com_servers[5],
+  ]
+}
